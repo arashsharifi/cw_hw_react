a79ced0e8485f6ddb6ca34afeee081de8c3f9c8c
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-10-18 13:59:55 +0330

    add Qcourse2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Qcourse1 from "./copmonents/Qcourse1";
+import Qcourse2 from "./copmonents/Qcourse2";
 
 function App() {
   return (
     <div className="container-page">
       <h1>Qcourse_1</h1>
       <Qcourse1></Qcourse1>
+      <h1>Qcourse_2</h1>
+      <Qcourse2></Qcourse2>
     </div>
   );
 }
